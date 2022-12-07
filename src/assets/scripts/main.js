4cb7d6d7000b54816b85c0dc56781c0f82a3bf00
Xavier Baca Garcia <xbaca@uoc.edu>
2022-12-07 20:15:52 +0100

    pac2 commit

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,22 @@
+/**
+ * Import dependencies from node_modules
+ * see commented examples below
+ */
+
+// import 'some-node-module';
+// import SomeModule from 'some-node-module';
+
+// Import all of Bootstrap's JS
+//import * as bootstrap from 'bootstrap';
+
+/**
+ * Write any other JavaScript below
+ */
+ 
+
++( function() {
+  const university = "UOC";
+  console.log(`Hello, ${university}!`);
+} )();
+
+
